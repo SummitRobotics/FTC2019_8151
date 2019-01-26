2a7279ce3b9218618aa752d206e4c49e727dcd42
Unknown <aidanb1049@gmail.com>
2019-01-26 08:41:13 -0800

    Wrote auto depot side with encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAuto.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.autonomous.actions.CoreAction;
 import org.firstinspires.ftc.teamcode.autonomous.actions.LiftMotorControl;
 import org.firstinspires.ftc.teamcode.autonomous.actions.MoveByEncoder;
+import org.firstinspires.ftc.teamcode.autonomous.actions.TurnByEncoder;
+
 import java.util.ArrayList;
 
 @Autonomous(name="TestAuto", group="LinearOpMode")
@@ -16,8 +18,13 @@ public class TestAuto extends CoreAuto {
     @Override
     public void runOpMode() {
         // Add paths for autonomous
-        path.add(new LiftMotorControl(3.5, 1, END));
-
+        path.add(new LiftMotorControl(3.5, 1, 1));
+        path.add(new TurnByEncoder(-0.2222 * Math.PI, .5,1));
+        path.add(new MoveByEncoder(-3,1,1));
+        path.add(new TurnByEncoder(0.2222 * Math.PI,.5,1));
+        path.add(new MoveByEncoder(-3,1,1));
+        path.add(new TurnByEncoder(0.5555 * Math.PI,.5,1));
+        path.add(new MoveByEncoder(132,1,END));
 
         // Update telemetry
         telemetry.addData("Status", "Initialized");
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DillonTeleop.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.main.Hardware;
+
+@Autonomous(name = "Dillon Teleop", group = "Linear Opmode")
+public class DillonTeleop extends LinearOpMode {
+    private Hardware robot = new Hardware();
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
