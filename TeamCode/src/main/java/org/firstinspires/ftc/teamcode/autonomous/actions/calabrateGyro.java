489014a07cf552ed72b38cd47340959aceb7a87e
django <django@inventobot.com>
2019-02-04 21:00:15 -0800

    first step of spliting land into diffrent actions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/actions/calabrateGyro.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.autonomous.actions;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import static android.os.SystemClock.sleep;
+
+public class calabrateGyro extends CoreAction {
+
+    private double speed;
+    private double time;
+    ElapsedTime runtime = new ElapsedTime();
+
+    public Orientation calibratedAngles;
+
+
+    public calabrateGyro(int nextPos) {
+
+        this.nextPos = nextPos;
+    }
+
+    @Override
+    public void actionInit(HardwareMap hardwareMap, Telemetry telemetry) {
+
+        robot.init(hardwareMap);
+
+        // gyro things -------------------------------------------
+
+        // State used for updating telemetry
+        Orientation angles;
+        Acceleration gravity;
+
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+
+
+
+        robot.gyro.initialize(parameters);
+
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+
+        sleep(200);
+    }
+
+    @Override
+    public int run() {
+
+        calibratedAngles = robot.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        return nextPos;
+    }
+
+    @Override
+    public void actionEnd() {
+
+    }
+
+}
